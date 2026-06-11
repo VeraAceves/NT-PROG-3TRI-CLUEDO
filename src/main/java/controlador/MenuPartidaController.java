1946b8131daba3886fd8128a15bd838fd774c8ff
Verónica Aceves Gil <veronicacevesgil@gmail.com>
2026-06-11 10:38:47 +0200

    Poner orden

--- src/main/java/controlador/MenuPartidaController.java
+++ src/main/java/controlador/MenuPartidaController.java
@@ -2,47 +2,67 @@ package controlador;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import modelo.beans.Jugador;
 
 public class MenuPartidaController {
 	@FXML
 	private Button btnValidarJugador;
-	
+
 	@FXML
 	private Button btnNivel1;
-	
+
 	@FXML
 	private Button btnNivel2;
-	
+
 	@FXML
 	private Button btnNivel3;
-	
+
 	@FXML
 	private Button btnNivel4;
-	
+
 	@FXML
 	private Button btnNivel5;
-	
+
 	@FXML
 	private Button btnNivel6;
-	
+
+	@FXML
+	Text txtNombre;
+
 	@FXML
 	private void initialize() {
-		
+
 	}
-	
+
 	@FXML
 	private void validarNombre() {
-		
+		String nombre = txtNombre.getText();
+
+		Jugador jugador = new Jugador();
+		jugador.setNombre(nombre);
+
+		if (!jugador.nombreValido()) {
+			System.out.println("Nombre no válido");
+			return;
+		}
+
+		if (jugadorDAO.existeJugador(nombre)) {
+			mostrarError("Ese nombre ya existe");
+			return;
+		}
+
+		this.jugador = jugador;
 	}
-	
+
 	@FXML
 	private void seleccionarNivel() {
-		
+
 	}
-	
+
 	@FXML
 	private void iniciarPartida() {
-		
+
 	}
 
 }
--- src/module-info.java
+++
@@ -1,6 +0,0 @@
-module CluedoFan {
-	requires javafx.controls;
-	requires javafx.fxml;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
